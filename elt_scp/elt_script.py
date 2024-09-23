257184cfffb681fb4a9714ee967c297815c125e1
VectorTensor <comppro54@gmail.com>
2024-09-23 09:36:37 +0545

    etl udpate

--- elt_scp/elt_script.py
+++ elt_scp/elt_script.py
@@ -1,3 +1,20 @@
 import subprocess
 import time
 
+
+
+def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+    retries = 0 
+    while retries < max_retries:
+        try:
+            result = subprocess.run(
+                    ["pg_isready", "-h",host], check=True, capture_output=True, text=True
+                    )
+            if "accepting connections" in result.stdout:
+                print("Successfully connected to postgres")
+                return True
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error connecting to Postgres:{e}")
+            retries +=1
+            print(f"Retrying in {delay_seconds} ")
